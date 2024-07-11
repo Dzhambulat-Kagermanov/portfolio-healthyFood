e797b3fba465ae5c0df3f78b4c71bc96e4b0db48
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-07-11 12:35:57 +0300

    FIX: Fixed imports issues in BurgerButton ui component

--- src/shared/ui/BurgerButton/BurgerButton.tsx
+++ src/shared/ui/BurgerButton/BurgerButton.tsx
@@ -1,18 +1,18 @@
 import { ButtonHTMLAttributes, FC } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { IClassName } from 'shared/types/shared'
-import classes from './MenuButton.module.scss'
+import classes from './BurgerButton.module.scss'
 
-interface IMenuButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, IClassName {}
+interface IBurgerButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, IClassName {}
 
-const BurgerButton: FC<IMenuButtonProps> = ({ onClick, className }) => {
-    return (
-        <button className={classNames(classes.btn, {}, [className])}>
-            <span></span>
-            <span></span>
-            <span></span>
-        </button>
-    )
+const BurgerButton: FC<IBurgerButtonProps> = ({ onClick, className }) => {
+  return (
+    <button className={classNames(classes.btn, {}, [className])}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  )
 }
 
 export default BurgerButton
