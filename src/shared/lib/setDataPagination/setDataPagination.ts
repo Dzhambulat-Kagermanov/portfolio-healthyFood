707d7f7d462a7d92e717189363117ef951c56d37
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-07-13 00:02:02 +0300

    UPDATE: Add comments for dishes pagination

--- src/shared/lib/setDataPagination/setDataPagination.ts
+++ src/shared/lib/setDataPagination/setDataPagination.ts
@@ -3,10 +3,15 @@ import { getDataPagination } from '../getDataPagination/getDataPagination'
 import { getDataTimeout } from '../getDataTimeout/getDataTimeout'
 
 export function setDataPagination<T>(data: T[], step: number) {
+  // Сколько элементов загружается за раз
   const paginationStep: number = step
+  // Сдвиг для пагинации
   let [paginationPosition, setPaginationPosition] = useState<number>(0)
+  // Данные загрузились или грузятся для лоадера
   const [isLoading, setIsLoading] = useState<boolean>(false)
+  // Данные которые загрузились и которые можно отрисовать
   const [loadingData, setLoadingData] = useState<typeof data>(getDataPagination(data, 0, paginationStep))
+  // Есть ли еще данные для загрузки
   const [isLoadData, setIsLoadData] = useState<boolean>(true)
 
   return {
